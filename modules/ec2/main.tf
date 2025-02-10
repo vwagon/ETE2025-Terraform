8b1c6fb412261c5375654b729380e4b4d838d99c
Valentin <dmtvalentin@gmail.com>
2025-02-10 21:22:20 +0100

    Ajout de sudo

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,8 +17,8 @@ resource "aws_instance" "ec2_java" {
         sudo docker --version
         sudo java -version
         sudo usermod -aG docker ec2-user
-        docker ps
         sudo systemctl enable --now docker
+        sudo docker ps
         sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
         docker-compose version

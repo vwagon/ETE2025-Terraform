4a9492efae08a1380193d02e45372a2d54ad851c
Valentin <dmtvalentin@gmail.com>
2025-02-10 17:50:48 +0100

    Changement installation docker-compose

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,12 +17,11 @@ resource "aws_instance" "ec2_java" {
         sudo docker --version
         sudo java -version
         sudo usermod -aG docker ec2-user
+        docker ps
         systemctl enable --now docker
-        DOCKER_CONFIG=$${DOCKER_CONFIG:-/usr/local/lib/docker}
-        mkdir -p $DOCKER_CONFIG/cli-plugins
-        curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-        chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-        ln -s $DOCKER_CONFIG/cli-plugins/docker-compose /usr/bin/docker-compose
+        sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        docker-compose version
 	EOF
 
 }

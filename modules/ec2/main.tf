a852199873d3325516d304c59ee62aa461ee0d86
Valentin <dmtvalentin@gmail.com>
2025-02-12 21:52:33 +0100

    Ajout d'un script distinct pour le userdata

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -7,24 +7,7 @@ resource "aws_instance" "ec2_java" {
     tags = {
         Name = "ecf_ec2"
     }
-    user_data = <<-EOF
-		#!/bin/bash
-        set -e
-        sudo yum update -y
-		    sudo yum install -y java mariadb105 git maven
-        sudo yum install -y docker
-        sudo docker --version
-        sudo java -version
-        sudo usermod -aG docker ec2-user
-        sudo systemctl enable --now docker
-        sudo systemctl status docker
-        sudo docker ps
-        sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-        docker-compose version
-        sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-	EOF
-
+    user_data = file("dependance_ec2.sh") 
 }
 
 resource "aws_key_pair" "ecfkey" {

bbd152447bcd3bb85092041ca417858424340f87
Valentin <dmtvalentin@gmail.com>
2025-02-18 22:01:30 +0100

    maj sg

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "ec2_sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = [0.0.0.0/0]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         description = "Grafana"

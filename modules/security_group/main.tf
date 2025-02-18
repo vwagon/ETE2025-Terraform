85d89326d790460f4327cb535a10ecc26e064515
Valentin <dmtvalentin@gmail.com>
2025-02-18 17:24:20 +0100

    Remodelisation du security group pour + de securite

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,21 +1,57 @@
+locals {
+  public_subnet_cidr  = "10.0.1.0/24"    # EC2 subnet
+  private_subnet_cidr = "10.0.2.0/24"    # RDS subnet
+  admin_ip           = ["91.160.175.100/32"]     # Your IP for SSH/Grafana
+}
+
 resource "aws_security_group" "ec2_sg" {
     name = "ec2_sg"
     vpc_id = var.vpc_id
-    description = "Autorise ssh et le traffic entrant"
+    description = "EC2 security group"
     tags = {
         Name = "ec2_sg"
     }
     ingress {
+        description = "SSH"
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = local.admin_ip
     }
     ingress {
+        description = "MySQL to RDS"
         from_port   = 3306
         to_port     = 3306
         protocol    = "tcp"
-        cidr_blocks = ["10.0.0.0/16"]
+        cidr_blocks = [aws_security_group.rds_sg.id]
+    }
+    ingress {
+        description = "Grafana"
+        from_port   = 3000
+        to_port     = 3000
+        protocol    = "tcp"
+        cidr_blocks = local.admin_ip
+    }
+    ingress {
+        description = "Prometheus"
+        from_port   = 9090
+        to_port     = 9090
+        protocol    = "tcp"
+        cidr_blocks = [local.private_subnet_cidr]
+    }
+    ingress {
+        description = "Loki"
+        from_port   = 3100
+        to_port     = 3100
+        protocol    = "tcp"
+        cidr_blocks = [local.private_subnet_cidr]
+    }
+    ingress {
+        description = "Promtail"
+        from_port   = 9080
+        to_port     = 9080
+        protocol    = "tcp"
+        cidr_blocks = [local.private_subnet_cidr]
     }
     egress {
         from_port   = 0
@@ -28,7 +64,7 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_security_group" "rds_sg" {
     name = "rds_sg"
     vpc_id = var.vpc_id
-    description = "Autorise les connexions MySQL depuis EC2"
+    description = "RDS security group"
     tags = {
       Name = "rds_sg"
     }
@@ -44,12 +80,5 @@ resource "aws_security_group" "rds_sg" {
         to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["10.0.0.0/16"]
-    }
-
-    ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["91.160.21.120/32"]
     }
 }

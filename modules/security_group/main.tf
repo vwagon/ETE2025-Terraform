a59aa3e14067dbd859e40bb058d8d1ea6587f194
Valentin <dmtvalentin@gmail.com>
2025-02-18 18:10:26 +0100

    Reouverture ssh pour verif

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "ec2_sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = local.admin_ip
+        cidr_blocks = [0.0.0.0/0]
     }
     ingress {
         description = "Grafana"
